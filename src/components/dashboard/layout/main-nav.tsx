67b3e6a20b301af61bae43750b54db423fadaab1
aivko <ivko.anton@pdffiller.team>
2024-05-25 16:42:36 +0300

    [Features] - update css styles

--- src/components/dashboard/layout/main-nav.tsx
+++ src/components/dashboard/layout/main-nav.tsx
@@ -29,7 +29,7 @@ export function MainNav({ user }: any): React.JSX.Element {
           backgroundColor: 'var(--mui-palette-background-paper)',
           position: 'sticky',
           top: 0,
-          zIndex: 'var(--mui-zIndex-appBar)',
+          zIndex: '100',
         }}
       >
         <Stack
