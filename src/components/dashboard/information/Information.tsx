5c084cd6d56d16eeb064280c8aa36c07de90b43e
aivko <ivko.anton@pdffiller.team>
2024-06-04 15:35:37 +0300

    update pdf

--- src/components/dashboard/information/Information.tsx
+++ src/components/dashboard/information/Information.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import PdfViewer from "@/components/dashboard/shared/PdfViewer";
 import Typography from "@mui/material/Typography";
-const pdfUrl = 'https://ik.imagekit.io/gjo0mtzlyq/pdf/COMPARATIVA_PREZZI_MAGGIO_2024.pdf?updatedAt=1715514543149';
+const pdfUrl = 'https://ik.imagekit.io/gjo0mtzlyq/pdf/COMPARATIVA_PREZZI_MAGGIO_2024.pdf';
 
 export default function Information () {
   return (
